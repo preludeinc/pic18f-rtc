8a4ef0e2937b5b2c5d468de805a0bdccc85d79f8
preludeinc <71677968+preludeinc@users.noreply.github.com>
2021-02-10 14:51:12 -0800

    Initial Commit

---
+++ keypad.c
@@ -0,0 +1,67 @@
+/* 
+ * File:   keypad.c
+ */
+
+#include <xc.h>
+#include "lcd18f.h"
+#include "keypad.h"
+#include "clockcode.h"
+
+#pragma config OSC = HS, FCMEN = OFF, PWRT = ON, BOREN = OFF
+#pragma config WDT = OFF, MCLRE = ON, PBADEN = OFF, LVP = OFF
+
+#define _XTAL_FREQ 20000000 
+
+// function prototype
+unsigned char keypad(void);
+
+// switch case function which checks if matrix keys are pressed
+unsigned char keypad(void)
+{	 
+  __delay_ms(30);                    // switch de-bounce delay 
+	
+  Nop();
+	Nop();
+    
+	LATBbits.LATB0 = 0;                 // 0 is rotated through lower nibble
+	LATBbits.LATB1 = 1;                 
+	LATBbits.LATB2 = 1;                 // internal weak pull-ups - checks which is low
+	LATBbits.LATB3 = 1;
+	Nop();
+	Nop();
+	
+  if(!PORTBbits.RB4)return('1');      // checking the first row of the keypad
+	if(!PORTBbits.RB5)return('4');
+	if(!PORTBbits.RB6)return('7');
+	if(!PORTBbits.RB7)return('*');
+	LATBbits.LATB0 = 1;
+	LATBbits.LATB1 = 0;
+	Nop();
+	Nop();
+	
+  if(!PORTBbits.RB4)return('2');      // checking the second row of the keypad
+	if(!PORTBbits.RB5)return('5');
+	if(!PORTBbits.RB6)return('8');
+	if(!PORTBbits.RB7)return('0');
+	LATBbits.LATB1 = 1;
+	LATBbits.LATB2 = 0;
+	Nop();
+	Nop();
+	
+  if(!PORTBbits.RB4)return('3');      // checking the third row of the keypad
+	if(!PORTBbits.RB5)return('6');	
+	if(!PORTBbits.RB6)return('9');
+	if(!PORTBbits.RB7)return('#');
+	LATBbits.LATB2 = 1;
+	LATBbits.LATB3 = 0;
+	Nop();
+	Nop();
+	
+    if(!PORTBbits.RB4)return('A');    // checking the fourth row of the keypad
+	if(!PORTBbits.RB5)return('B');
+	if(!PORTBbits.RB6)return('C');
+	if(!PORTBbits.RB7)return('D');
+
+	return(0);                         // no valid key pressed, exit switch case
+}
+
